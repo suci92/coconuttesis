5e4bd706c182422d0fca682b40816cddd460a36a
suci92 <155895806+suci92@users.noreply.github.com>
2024-01-07 21:45:46 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,10 @@ os.makedirs('temps/', exist_ok=True)
 # check if the file is uploaded
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
+if image = '*.jpg'
     temp_image_path = 'temps/temp_image.jpg'
+else
+    temp_image_path = 'temps/temp_image.png'
     image.save(temp_image_path)
     st.image(image, caption='Uploaded Image', use_column_width=True)
     
